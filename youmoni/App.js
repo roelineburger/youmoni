29800093d5be39cbfdbd14b97b5ef0d88cc7a322
roelineburger <93773777+roelineburger@users.noreply.github.com>
2023-10-10 12:18:19 +0200

    feat: Add barcode scanner

--- youmoni/App.js
+++ youmoni/App.js
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button, Image } from "react-native";
 import YouMoniLogo from "./assets/youmoni.png";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [barcodeScannerVisible, setBarcodeScannerVisible] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    console.log(data);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
 
   const toggleBarcodeScanner = () => {
     setBarcodeScannerVisible(!barcodeScannerVisible);
     setScanned(false);
   };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={styles.container}>
@@ -29,6 +53,16 @@ export default function App() {
           accessibilityLabel="Scan QR Code"
         />
       </View>
+
+      {barcodeScannerVisible && (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      )}
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
     </View>
   );
 }
